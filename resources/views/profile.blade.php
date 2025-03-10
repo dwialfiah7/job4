8eea25df01b9e9a5d4c100a317c1ec76beb67113
dwialfiah7 <dwialfiah66@gmail.com>
2025-03-10 12:39:40 +0700

    Pesan Commit

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="Container mt-4">
+            <div class="card">
+                <div clas="card-header">
+                    <h1>Profil Saya</h1>
+                </div>
+                <div class="card-body">
+                    <img src="poto.jpeg" alt="profile picture" width="150">
+                    <p>Nama: Dwi Alfiah Wahyuningrum</p>
+                    <p>Email: dwialfiah48@gmail.com</p>
+                    <p>Bio: Pelajar</p>
+                    <h5>Skills:</h5>
+                    <ul>
+                        <li>HTML</li>
+                        <li>PHP</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </body>    
+</html>
